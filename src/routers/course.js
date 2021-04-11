13c507c0e5a689e8f85238b41cb6996951644f05
sushantbaskota2 <sushantbaskota2@gmail.com>
2021-04-10 21:07:56 -0500

    some changes

--- src/routers/course.js
+++ src/routers/course.js
@@ -62,26 +62,47 @@ router.get('/courses/:author', auth, async (req, res) => {
 });
 
 router.get('/course/:id', async (req, res) => {
+    console.log(req.params.id);
     try {
-        const courses = Course.findById(req.params.id);
+        const courses = await Course.findById(req.params.id);
         res.send(courses);
-    } catch (error) {}
+    } catch (error) {
+        res.send(error);
+        console.log(error);
+    }
 });
 //Enroll a Student in a course --NEEDS AUTHENTICATION ---
 
-router.post('/courses/enroll/:courseId', auth, async (req, res) => {
+const enrollStudent = async (studentEmail, courseId) => {
+    console.log(studentEmail);
+    const student = await User.findOne({ email: studentEmail });
+    student.courses.addToSet(courseId);
+    console.log(student);
+    await student.save();
+};
+
+router.post('/courses/enroll/:courseId', async (req, res) => {
     try {
-        const course = await Course.findById(req.params.courseId);
+        for (let i = 0; i < req.body.studentEmails.length; i++) {
+            enrollStudent(req.body.studentEmails[i], req.params.courseId);
+        }
 
-        const user = await User.findById(req.body.studentId);
-
-        user.courses.addToSet(req.params.courseId);
-        await user.save();
-        console.log(user);
-        res.send(user);
+        res.send({ message: 'success' });
     } catch (error) {
         console.log(error);
         res.status(400).send(error);
+    }
+});
+
+router.post('/courses/unenroll/:courseId', async (req, res) => {
+    try {
+        const user = await User.findById(req.body.studentId);
+        user.courses = user.courses.filter((course) => course != req.params.courseId);
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
     }
 });
 
--- src/routers/submission.js
+++ src/routers/submission.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const axios = require('axios');
 const Submission = require('../db/models/submission');
+const User = require('../db/models/user');
 const Problem = require('../db/models/problem');
+const Course = require('../db/models/course');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const CLIENT_ID = process.env.JDOODLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.JDOODLE_CLIENT_SECRET;
 const baseURL = 'https://api.jdoodle.com/v1/execute';
+
+router.get('/instructor/submissions', auth, async (req, res) => {
+    const problems = await Problem.find({ author: req.user._id });
+    let submissions = [];
+    for (let i = 0; i < problems.length; i++) {
+        const submission = await Submission.find({ problemId: problems[i]._id });
+        const submissionInfo = [];
+        for (const a of submission) {
+            const user = await User.findById(a.studentId);
+            const course = await Course.findById(problems[i].courseID);
+            submissionInfo.push({
+                name: user.name,
+                problemName: problems[i].title,
+                courseID: course.courseID,
+                ...a._doc
+            });
+        }
+        submissions.push(...submissionInfo);
+    }
+    res.send(submissions);
+});
 
 router.post('/solve', async (req, res) => {
     const { studentCode, testcases } = req.body;
@@ -46,6 +70,26 @@ router.get('/submissions', async (req, res) => {
     }
 });
 
+router.get('/submissions/student', auth, async (req, res) => {
+    try {
+        const submissions = await Submission.find({ studentId: req.user._id });
+        let submissionInfo = [];
+        for (a of submissions) {
+            const problem = await Problem.findById(a.problemId);
+            const course = await Course.findById(problem.courseID);
+            submissionInfo.push({
+                ...a._doc,
+                title: problem.title,
+                courseID: course.courseID
+            });
+        }
+        res.send(submissionInfo);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
 let code = `
 const student = (input)=>{
     return input.split('').reverse().join('');
@@ -74,7 +118,7 @@ const runCodeThroughAPI = async ({ code, testcase }) => {
             versionIndex: '3',
             stdin: ''
         });
-        return res.data.output;
+        return res.data;
     } catch (error) {
         console.log(error);
     }
@@ -85,10 +129,10 @@ const verifyTestCases = async ({ code, testcases }) => {
     for (let i = 0; i < testcases.length; i++) {
         let hamroResponse = await runCodeThroughAPI({ code, testcase: testcases[i] });
         console.log(hamroResponse);
-        if (hamroResponse.includes('true')) {
-            o.push('TRUE');
-        } else if (hamroResponse.includes('false')) {
-            o.push('FALSE');
+        if (hamroResponse.output.includes('true')) {
+            o.push({ status: 'TRUE', res: hamroResponse });
+        } else if (hamroResponse.output.includes('false')) {
+            o.push({ status: 'FALSE', res: hamroResponse });
         } else {
             o.push(hamroResponse);
         }
